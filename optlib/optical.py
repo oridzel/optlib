3739508a7ef02c61fd26a18724490257e234de85
olgaridzel <olgaridzel@yandex.ru>
2022-08-01 08:25:08 +0300

    eps del

--- optlib/optical.py
+++ optlib/optical.py
@@ -15,7 +15,6 @@ hc = 12.3981756608  # planck constant times velocity of light keV Angstr
 r0 = 2.8179403227e-15
 h2ev = 27.21184  # Hartree, converts Hartree to eV
 a0 = 0.529177  # Bohr Radius in Angstroem
-machine_eps = np.finfo('float64').eps
 wpc = 4*math.pi * a0**3
 avogadro = 6.02217e23
 c = 137.036
