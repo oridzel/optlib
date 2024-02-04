ade4b387871cf4f8f879b461a4854265789c7389
Olga <olga.yu.ridzel@gmail.com>
2024-02-04 15:48:14 -0500

    seemc progress bar

--- optlib/seemc.py
+++ optlib/seemc.py
@@ -7,15 +7,8 @@ from scipy.interpolate import RectBivariateSpline
 import random
 import matplotlib.pyplot as plt
 import time
+from tqdm import tqdm
 
-class RealNumber:
-    def __get__(self, obj, objtype = None):
-        return obj._value
-
-    def __set__(self, obj, value):
-        if isinstance(value, complex):
-            raise ValueError("The value must be real")
-        obj._value = value
 
 class Sample:
     material_data = {}
@@ -89,12 +82,14 @@ class Electron:
             self.coordinates.append(coord_vector)
         self.inside = True
         self.dead = False
-        self.scattering_type = 0
+        self.scattering_type = -1
         self.n_secondaries = 0
         self.energy_se = self.sample.material_data['e_fermi']
         self.energy_loss = 0
         self.path_length = 0
         self.deflection = [0,0]
+        self.sc_type_list = []
+        # self.print_info()
 
     def __str__(self):
         return f"Electron in {self.matname} with energy {self.energy} eV."
@@ -128,10 +123,33 @@ class Electron:
             raise ValueError("The sample must be of type Sample")
         self._sample = value
 
+    def print_info(self):
+        print("Sample:",self.sample)
+        print("Inner potential:",self.inner_potential)
+        print("Energy reference:",self.conduction_band_reference)
+        print("Track coordinates:",self.save_coordinates)
+        print("xyz:",self.xyz)
+        print("uvw:",self.uvw)
+        print("Generation:",self.generation)
+        print("Is secondary:",self.is_secondary)     
+        print("Energy:",self.energy)       
+        print("Initial energy:",self.initial_energy)
+        print("Parent index:",self.parent_index)
+        print("Coordinates:",self.coordinates)       
+        print("Inside:",self.inside)
+        print("Is dead:",self.dead)
+        print("Scat type:",self.scattering_type)
+        print("Number se:",self.n_secondaries)
+        print("Energy se:",self.energy_se)
+        print("Energy loss:",self.energy_loss)
+        print("Pathlength:",self.path_length)
+        print("Deflection:",self.deflection)
+        print("Scat type list:",self.sc_type_list)
+
     def travel(self):
         s = -(1/self.itmfp)*np.log(random.random())
-        # if self.xyz[2] + self.uvw[2]*s < 0:
-        #     s = np.abs(self.xyz[2]/self.uvw[2]) + 0.0001
+        if self.xyz[2] + self.uvw[2]*s < 0:
+            s = np.abs(self.xyz[2]/self.uvw[2]) + 0.0001
         self.path_length += s
         self.xyz[0] = self.xyz[0] + self.uvw[0]*s
         self.xyz[1] = self.xyz[1] + self.uvw[1]*s
@@ -148,95 +166,152 @@ class Electron:
             self.scattering_type = 1
         else:
             self.scattering_type = 2
+        self.sc_type_list.append(self.scattering_type)
 
     def scatter(self):
         self.deflection[1] = random.random()*2*math.pi
+        ind = np.absolute(self.sample.material_data['energy'] - self.energy).argmin()
         if self.scattering_type == 0:
-            loss = False
             # decs = self.sample.get_decs(self.energy)
-            ind = np.absolute(self.sample.material_data['energy'] - self.energy).argmin()
             decs = np.squeeze(self.sample.material_data['decs'][:,ind])
             cumsigma = integrate.cumtrapz(2*math.pi*decs*np.sin(self.sample.material_data['decs_theta']),self.sample.material_data['decs_theta'],initial=0)
             self.deflection[0] = np.interp(random.random()*self.sample.material_data['sigma_el'][ind],cumsigma,self.sample.material_data['decs_theta'])
-            self.uvw = self.update_direction(self.uvw,self.deflection)
+            self.uvw = self.update_direction(self.uvw,self.deflection,0)
+            # self.uvw = self.change_direction(self.uvw,self.deflection)
+            return False
         elif self.scattering_type == 1:
-            loss = True
             # [eloss,diimfp] = self.sample.get_diimfp(self.energy)
-            ind = np.absolute(self.sample.material_data['energy'] - self.energy).argmin()
             eloss = np.squeeze(self.sample.material_data['diimfp'][:,0,ind])
             diimfp = np.squeeze(self.sample.material_data['diimfp'][:,1,ind])
             cumdiimfp = integrate.cumtrapz(diimfp,eloss,initial=0)
             cumdiimfp = (cumdiimfp - cumdiimfp[0])/(cumdiimfp[-1] - cumdiimfp[0])
             self.energy_loss = np.interp(random.random(),cumdiimfp,eloss)
-            self.energy -= self.energy_loss
+            self.energy = self.energy - self.energy_loss
             self.is_dead()
             if not self.dead:
                 theta,angdist = self.sample.get_angular_iimfp(self.energy + self.energy_loss,self.energy_loss)
-                cumdiimfp = integrate.cumtrapz(angdist,theta,initial=0)
+                cumdiimfp = integrate.cumtrapz(angdist*np.sin(theta),theta,initial=0)
                 self.deflection[0] = np.interp(random.random()*cumdiimfp[-1],cumdiimfp,theta)
                 if self.deflection[0] == np.nan:
-                    self.deflection[0][0] = math.asin(math.sqrt(self.energy_loss/(self.energy + self.energy_loss)))
-                self.uvw = self.update_direction(self.uvw,self.deflection)
-        return loss
+                    print("nan")
+                    self.deflection[0] = math.asin(math.sqrt(self.energy_loss/(self.energy + self.energy_loss)))
+                self.uvw = self.update_direction(self.uvw,self.deflection,0)
+                # self.uvw = self.change_direction(self.uvw,self.deflection)
+            return True
+        else:
+            print("else")
+            return False
 
     def is_dead(self):
         if self.energy < self.inner_potential:
             self.dead = True
 
-    def update_direction(self,uvw,deflection):
+    def update_direction(self,uvw,deflection,algorithm=0):
         base_uvw = [0.0, 0.0, 0.0]
-        sdt = math.sin(deflection[0])
-        cdt = math.cos(deflection[0])
-        sdf = math.sin(deflection[1])
-        cdf = math.cos(deflection[1])
-        dxy = uvw[0]**2 + uvw[1]**2
-        dxyz = dxy + uvw[2]**2
-        if np.abs(dxyz - 1.0) > 1e-9:
-            fnorm = 1.0/math.sqrt(dxyz)
-            uvw[0] = fnorm*uvw[0]
-            uvw[1] = fnorm*uvw[1]
-            uvw[2] = fnorm*uvw[2]
+        if algorithm == 0:
+            sdt = math.sin(deflection[0])
+            cdt = math.cos(deflection[0])
+            sdf = math.sin(deflection[1])
+            cdf = math.cos(deflection[1])
             dxy = uvw[0]**2 + uvw[1]**2
-
-        if dxy > 1e-9:
-            sdt = math.sqrt((1.0 - cdt**2)/dxy)
-            up = uvw[0]
-            base_uvw[0] = uvw[0]*cdt + sdt*(up*uvw[2]*cdf - uvw[1]*sdf)
-            base_uvw[1] = uvw[1]*cdt + sdt*(uvw[1]*uvw[2]*cdf + up*sdf)
-            base_uvw[2] = uvw[2]*cdt - dxy*sdt*cdf
-        else:
-            sdt = math.sqrt(1.0-cdt**2)
-            base_uvw[1] = sdt*sdf
-            if uvw[2] > 0: 
-                base_uvw[0] = sdt*cdf
-                base_uvw[2] = cdt
+            dxyz = dxy + uvw[2]**2
+            if abs(dxyz - 1.0) > 1e-9:
+                fnorm = 1.0/math.sqrt(dxyz)
+                uvw[0] = fnorm*uvw[0]
+                uvw[1] = fnorm*uvw[1]
+                uvw[2] = fnorm*uvw[2]
+                dxy = uvw[0]**2 + uvw[1]**2
+    
+            if dxy > 1e-9:
+                sdt = math.sqrt((1.0 - cdt**2)/dxy)
+                up = uvw[0]
+                base_uvw[0] = uvw[0]*cdt + sdt*(up*uvw[2]*cdf - uvw[1]*sdf)
+                base_uvw[1] = uvw[1]*cdt + sdt*(uvw[1]*uvw[2]*cdf + up*sdf)
+                base_uvw[2] = uvw[2]*cdt - dxy*sdt*cdf
             else:
-                base_uvw[0] = -1.0*sdt*cdf
-                base_uvw[2] = -1.0*cdt
+                sdt = math.sqrt(1.0-cdt**2)
+                base_uvw[1] = sdt*sdf
+                if uvw[2] > 0: 
+                    base_uvw[0] = sdt*cdf
+                    base_uvw[2] = cdt
+                else:
+                    base_uvw[0] = -1.0*sdt*cdf
+                    base_uvw[2] = -1.0*cdt
+        elif algorithm == 1:
+            if abs(uvw[2]) > 0.99:
+                base_uvw[0] = math.sin(deflection[0])*math.cos(deflection[1])
+                base_uvw[1] = math.sin(deflection[0])*math.sin(deflection[1])
+                base_uvw[2] = math.cos(deflection[0])
+            else:
+                sq1 = math.sqrt(1 - math.cos(deflection[0])**2)
+                sqw = math.sqrt(1 - uvw[2]**2)
+                base_uvw[0] = uvw[0]*math.cos(deflection[0]) + sq1/sqw*(uvw[0]*uvw[2]*math.cos(deflection[1]) - uvw[1]*math.sin(deflection[1]))
+                base_uvw[1] = uvw[1]*math.cos(deflection[0]) + sq1/sqw*(uvw[1]*uvw[2]*math.cos(deflection[1]) + uvw[0]*math.sin(deflection[1]))
+                base_uvw[2] = uvw[2]*math.cos(deflection[0]) - sq1*sqw*math.cos(deflection[1])
+        else:
+            theta_old = math.acos(uvw[2])
+            phi_old = math.atan2(uvw[1],uvw[0])
+            theta = math.acos(math.cos(theta_old)*math.cos(deflection[0]) - math.sin(theta_old)*math.sin(deflection[0])*math.cos(deflection[1]))
+            phi = math.asin(math.sin(deflection[0])*math.sin(deflection[1])/math.sin(theta)) + phi_old
+
+            base_uvw[0] = math.sin(theta)*math.cos(phi)
+            base_uvw[1] = math.sin(theta)*math.sin(phi)
+            base_uvw[2] = math.cos(theta)
+            
         return base_uvw
+
+    def change_direction(self,uvw,deflection):
+        new_uvw = [0.0, 0.0, 0.0]
+        sin_psi = math.sin(deflection[0])
+        cos_psi = math.cos(deflection[0])
+        sin_fi = math.sin(deflection[1])
+        cos_fi = math.cos(deflection[1])
+        cos_theta = uvw[2]
+        sin_theta = math.sqrt(uvw[0]**2 + uvw[1]**2)
+        if sin_theta > 1e-10:
+            cos_phi = uvw[0] / sin_theta
+            sin_phi = uvw[1] / sin_theta
+        else:
+            cos_phi = 1.0
+            sin_phi = 0.0
+        # calculate new direction
+        h0 = sin_psi * cos_fi
+        h1 = sin_theta * cos_psi + h0 * cos_theta
+        h2 = sin_psi * sin_fi
+        new_uvw[0] = h1 * cos_phi - h2 * sin_phi
+        new_uvw[1] = h1 * sin_phi + h2 * cos_phi
+        new_uvw[2] = cos_theta * cos_psi - h0 * sin_theta
+        # to guarantee  dirx**2 + diry**2 + dirz**2 = 1
+        delta = 1.5 - 0.5 * (new_uvw[0]**2 + new_uvw[1]**2 + new_uvw[2]**2)
+        new_uvw[0] *= delta
+        new_uvw[1] *= delta
+        new_uvw[2] *= delta
+        return new_uvw
+        
+
+    def calculate_direction(self,theta,phi):
+        self.uvw[0] = math.sin(theta)*math.cos(phi)
+        self.uvw[1] = math.sin(theta)*math.sin(phi)
+        self.uvw[2] = -math.cos(theta)
+        norm = math.sqrt(self.uvw[0]**2 + self.uvw[1]**2 + self.uvw[2]**2)
+        self.uvw[0] /= norm
+        self.uvw[1] /= norm
+        self.uvw[2] /= norm
 
     def escape(self):
         theta = math.acos(self.uvw[2])
         phi = math.atan2(self.uvw[1],self.uvw[0])
         if self.xyz[2] < 0:
-            beta = math.pi - theta
-            ecos = self.energy*beta**2
+            ecos = self.energy*self.uvw[2]**2
             if ecos > self.inner_potential:
-                t = 4*math.sqrt(1 - self.inner_potential/ecos)/(1 + math.sqrt(1 - self.inner_potential/ecos))**2
+                t = 4*math.sqrt(1 - self.inner_potential/ecos)/((1 + math.sqrt(1 - self.inner_potential/ecos))**2)
             else:
                 t = 0
             if random.random() < t:
                 self.inside = False
-                self.xyz[0] += math.sin(beta)*math.cos(phi)*self.xyz[2]/math.cos(beta)
-                self.xyz[1] += math.sin(beta)*math.sin(phi)*self.xyz[2]/math.cos(beta)
-                self.xyz[2] = 0.0
-                if self.save_coordinates:
-                    self.coordinates[-1] = [ round(elem, 2) for elem in self.xyz + [self.energy] ]
-                theta = np.pi - np.arcsin(np.sin(beta)*np.sqrt( self.energy/(self.energy - self.inner_potential) ))                
+                theta_vacuum = math.asin(math.sin(theta)*math.sqrt( self.energy/(self.energy - self.inner_potential) ))
                 self.energy -= self.inner_potential
-                self.uvw[0] = math.sin(theta)*math.cos(phi)
-                self.uvw[1] = math.sin(theta)*math.sin(phi)
-                self.uvw[2] = math.cos(theta)
+                self.calculate_direction(theta_vacuum,phi)
                 if self.save_coordinates:
                     self.xyz[0] += 100*self.uvw[0]
                     self.xyz[1] += 100*self.uvw[1]
@@ -245,50 +320,57 @@ class Electron:
                     self.coordinates.append(coord_vector)
             else:
                 self.uvw[2] *= -1
-                self.xyz[2] *= -1
+                self.xyz[2] = 1e-10
                 if self.save_coordinates:
                     coord_vector = [ round(elem, 2) for elem in self.xyz + [self.energy] ]
                     self.coordinates.append(coord_vector)
 
 class SEEMC:
-    def __init__(self,energy_array,sample_name,n_traj,cb_ref,track):
+    def __init__(self,energy_array,sample_name,angle,n_traj,cb_ref,track):
         self.energy_array = energy_array
         self.sample = Sample(sample_name)
         self.n_trajectories = n_traj
         self.cb_ref = cb_ref
         self.track_trajectories = track
         self.electron_list = []
+        self.incident_angle = angle
 
-    def run_simulation(self):
+    def run_simulation(self,plot_yields = False):
         start_time = time.time()
         
         for energy in self.energy_array:
             print(energy)
             e_statistics = []
-            i = -1
-            for traj in range(self.n_trajectories):
-                e_statistics.append(Electron(self.sample,energy,self.cb_ref,self.track_trajectories,[0,0,0],[0,0,1],0,False,-1))
-                while i < len(e_statistics)-1:
+            i = 0
+            for traj in tqdm(range(self.n_trajectories)):
+                e_statistics.append(Electron(self.sample,energy,self.cb_ref,self.track_trajectories,[0,0,0],[math.sin(self.incident_angle),0,math.cos(self.incident_angle)],0,False,-1))
+                while i < len(e_statistics):
+                    while e_statistics[i].inside and not e_statistics[i].dead:
+                        se_xyz = [0,0,0]
+                        se_uvw = [0,0,0]
+                        e_statistics[i].travel()
+                        e_statistics[i].escape()
+                        if e_statistics[i].inside and not e_statistics[i].dead:
+                            e_statistics[i].get_scattering_type()
+                            if e_statistics[i].scatter():
+                                se_energy = e_statistics[i].energy_loss + e_statistics[i].sample.material_data['e_fermi']
+                                if se_energy > e_statistics[i].inner_potential:
+                                    se_xyz[0] = e_statistics[i].xyz[0]
+                                    se_xyz[1] = e_statistics[i].xyz[1]
+                                    se_xyz[2] = e_statistics[i].xyz[2]
+                                    se_uvw = e_statistics[i].update_direction(e_statistics[i].uvw,[math.asin(math.cos(e_statistics[i].deflection[0])),e_statistics[i].deflection[1] + math.pi])
+                                    e_statistics[i].n_secondaries += 1
+                                    e_statistics.append(Electron(self.sample,se_energy,self.cb_ref,self.track_trajectories,se_xyz,se_uvw,e_statistics[i].generation + 1,True,i))
                     i += 1
-                    e = e_statistics[i]
-                    while e.inside and not e.dead:
-                        e.travel()
-                        e.escape()
-                        if e.inside and not e.dead:
-                            e.get_scattering_type()
-                            if e.scatter():
-                                se_energy = e.energy_loss + e.sample.material_data['e_fermi']
-                                if se_energy > e.inner_potential:
-                                    se_uvw = e.update_direction(e.uvw,[math.asin(math.cos(e.deflection[0])),e.deflection[1] + math.pi])
-                                    e.n_secondaries += 1
-                                    e_statistics.append(Electron(self.sample,se_energy,self.cb_ref,self.track_trajectories,e.xyz,se_uvw,e.generation + 1,True,i))
             self.electron_list.append(e_statistics)
-        
         print("--- %s seconds ---" % (time.time() - start_time))
+        if plot_yields:
+            self.calculate_yield()
+            self.plot_yield()
 
     def calculate_yield(self):
         self.tey = np.zeros(len(self.energy_array))
-        self.sey = np.zeros(len(self.energy_arrayy))
+        self.sey = np.zeros(len(self.energy_array))
         self.bse = np.zeros(len(self.energy_array))
         
         for i in range(len(self.energy_array)):
@@ -307,17 +389,41 @@ class SEEMC:
         print("SEY:",self.sey)
         print("BSE:",self.bse)
 
+    def calculate_energy_histogram(self,energy_index=0):
+        self.se_histogram = []
+        self.pe_histogram = []
+        for e in self.electron_list[energy_index]:
+            if not e.inside:
+                if e.is_secondary:
+                    self.se_histogram.append(e.energy)
+                else:
+                    self.pe_histogram.append(e.energy)
+
+    def plot_yield(self):
+        plt.figure()
+        plt.plot(self.energy_array,self.tey,label="TEY")
+        plt.plot(self.energy_array,self.sey,label="SEY")
+        plt.plot(self.energy_array,self.bse,label="BSE")
+        plt.xlabel('Energy (eV)')
+        plt.ylabel('Yield')
+        plt.title(self.sample.name)
+        plt.legend()
+        plt.show()
+
     def plot_trajectories(self,energy_index):
         plt.figure()
         energy = self.energy_array[energy_index]
         for e in self.electron_list[energy_index]:
+            # if not e.is_secondary:
             x_c = [x[0] for x in e.coordinates]
             y_c = [x[1] for x in e.coordinates]
             z_c = [x[2] for x in e.coordinates]
             c = [x[3] for x in e.coordinates]
-            # plt.plot(x_c,z_c,linewidth=1)
-            plt.scatter(y_c,z_c,s=5, c=c, cmap='jet')
+            plt.plot(x_c,z_c,'-o',linewidth=1)
+            # plt.scatter(y_c,z_c,s=5, c=c, cmap='jet')
         plt.gca().invert_yaxis()
-        plt.colorbar()
+        # plt.colorbar()
         plt.show()
+        
+        
         
