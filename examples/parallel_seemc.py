4387dc2f165940f327bbf462a26457fe4678be37
Olga Ridzel <olgaridzel@nist.gov>
2024-03-26 13:51:08 -0400

    list extension fixed

--- examples/parallel_seemc.py
+++ examples/parallel_seemc.py
@@ -4,26 +4,24 @@ import matplotlib.pyplot as plt
 
 num_cores = multiprocessing.cpu_count()
 energies = [10, 20, 50, 100, 200, 500, 1000]
-sey = []
-bsey = []
-
 
 def run_seemc(i):
     energy = energies[i]
     print(energy)
-    s = mc.SEEMC([energy], 'Au', 0, 1000, False, False)
+    s = mc.SEEMC([energy], 'Au', 0, 10000, False, False)
     s.run_simulation(False)
     s.calculate_yield()
-    sey.append(s.sey)
-    bsey.append(s.bse)
+    return s.sey[0], s.bse[0], s.sey[0]+s.bse[0]
 
 
 if __name__ == '__main__':
+
     with multiprocessing.Pool(num_cores) as pool:
-        pool.map(run_seemc, range(len(energies)))
+        res = pool.map(run_seemc, range(len(energies)))
+        sey, bsey, tey = zip(*res)
 
     plt.figure()
-    plt.plot(energies, sey+bsey, label='TEY')
+    plt.plot(energies, tey, label='TEY')
     plt.plot(energies, sey, label='SEY')
     plt.plot(energies, bsey, label='BSEY')
     plt.xlabel('Energy')
